b2e168cc3b738e2c6fb9eeecdbc9451dfa158911
David Cechak <DavidC@kentico.com>
2017-03-09 16:43:30 +0100

    KA-85 Create Item, ItemsControler and Get methods
    in ItemsControler

---
+++ ItemList/ItemList.Api/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ItemList.Api.Models;
+
+namespace ItemList.Api.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        private static List<Item> Items;
+
+        public IHttpActionResult GetItem(string id)
+        {
+            var Item = Items.Find((item) => item.Id == id);
+
+            if (Item != null) ? Ok(Item) : NotFound();
+        }
+    }
+}
---
+++ ItemList/ItemList.Api/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ItemList.Api.Models
+{
+    public class Item
+    {
+        [Required]  
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Value { get; set; }
+    }
+}
