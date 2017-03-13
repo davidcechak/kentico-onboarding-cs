de0bb5d7ba8453581b1df0d133503f141d5a15d3
Jiri Lojda <JiriL@kentico.com>
2017-03-13 15:53:43 +0100

    KA-85 Refactor ItemsController not to use List<Item> and return correct Location in Post

--- ItemList/ItemList.Api/Controllers/ItemsController.cs
+++ ItemList/ItemList.Api/Controllers/ItemsController.cs
@@ -11,41 +11,35 @@ namespace ItemList.Api.Controllers
 {
     public class ItemsController : ApiController
     {
-        public static List<Item> items = new List<Item>
-        {
-            new Item { Id = new Guid("7383243d-9230-4a6c-94ea-122e151208ca"), Value = "text1" },
-            new Item { Id = new Guid("83aa9154-2b5f-49b7-b7af-25cab7bf2159"), Value = "text2" },
-            new Item { Id = new Guid("331c43f5-11af-43a4-83d1-7d949ae5a8d7"), Value = "text3" },
-};
 
         public IEnumerable<Item> Get()
         {
-            yield return items[0];
-            yield return items[1];
+            yield return new Item { Id = new Guid("7383243d-9230-4a6c-94ea-122e151208ca"), Value = "text1" };
+            yield return new Item { Id = new Guid("83aa9154-2b5f-49b7-b7af-25cab7bf2159"), Value = "text2" };
         }
 
 
         public IHttpActionResult Get(Guid id)
         {
-            return Ok(items[2]);
+            var item = new Item { Id = new Guid("331c43f5-11af-43a4-83d1-7d949ae5a8d7"), Value = "text3" };
+            return Ok(item);
         }
 
 
         public IHttpActionResult Post(Item item)
         {
+            item = new Item { Id = new Guid("5081544A-5584-4449-B0CD-72B2BFF0AF30"), Value = "text4" };
+
             var location = Url.Link(null, new
             {
                 Controller = nameof(ItemsController).Replace("Controller", String.Empty),
-                Action = "Get",
-                Id = "331c43f5-11af-43a4-83d1-7d949ae5a8d7"
+                Id = item.Id
             });
 
-            items[2].Value = item.Value;
-
-            return Created(location, items[2]);
+            return Created(location, item);
         }
 
-        
+
         public IHttpActionResult Delete(Guid id)
         {
             return StatusCode(HttpStatusCode.NoContent);
