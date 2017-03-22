a015699986705d59bcfb44b334d81375e54f431a
Jiri Lojda <JiriL@kentico.com>
2017-03-22 17:25:46 +0100

    KA-86 Create GuidGenerator in new ServiceLayer project

---
+++ ItemList/ItemList.ServiceLayer/Utils/GuidGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ItemList.Contracts.ServiceLayer;
+
+namespace ItemList.ServiceLayer.Utils
+{
+    public class GuidGenerator : IGuidGenerator
+    {
+        public Guid GenerateGuid()
+        {
+            return new Guid();
+        }
+    }
+}
