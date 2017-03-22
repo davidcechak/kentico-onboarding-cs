3639ba89b3ec5d511d69df6071035241afd2ab79
Jiri Lojda <JiriL@kentico.com>
2017-03-22 12:56:55 +0100

    KA-86 Make ItemsRepository methods async

--- ItemList/ItemList.DatabaseLayer/Repositories/ItemsRepository.cs
+++ ItemList/ItemList.DatabaseLayer/Repositories/ItemsRepository.cs
@@ -6,36 +6,36 @@ using ItemList.Contracts.Models;
 
 namespace ItemList.DatabaseLayer.Repositories
 {
-    public class ItemsRepository : IItemsRepository
+    internal class ItemsRepository : IItemsRepository
     {
-        public Task<IEnumerable<Item>> GetAll()
+        public async Task<IEnumerable<Item>> GetAll()
         {
             IEnumerable<Item> items = new List<Item>()
             {
                 new Item {Id = new Guid("7383243d-9230-4a6c-94ea-122e151208ca"), Value = "text1"},
                 new Item {Id = new Guid("83aa9154-2b5f-49b7-b7af-25cab7bf2159"), Value = "text2"}
             };
-            return Task.FromResult(items);
+            return await Task.FromResult(items);
         }
 
-        public Task<Item> Get()
+        public async Task<Item> Get()
         {
-            return Task.FromResult(new Item { Id = new Guid("331c43f5-11af-43a4-83d1-7d949ae5a8d7"), Value = "text3" });
+            return await Task.FromResult(new Item { Id = new Guid("331c43f5-11af-43a4-83d1-7d949ae5a8d7"), Value = "text3" });
         }
 
-        public Task Create()
+        public async Task Create()
         {
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
 
-        public Task Update()
+        public async Task Update()
         {
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
 
-        public Task Delete()
+        public async Task Delete()
         {
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
     }
 }
