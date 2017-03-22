43653b4426a889d918335aa52590356ab6dcacfc
Jiri Lojda <JiriL@kentico.com>
2017-03-22 11:55:45 +0100

    KA-86 Create IItemsRepository

---
+++ ItemList/ItemList.Contracts/DatabaseLayer/IItemsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ItemList.Contracts.Models;
+
+namespace ItemList.Contracts.DatabaseLayer
+{
+    public interface ItemsRepository
+    {
+        Task<IEnumerable<Item>> GetAll();
+        Task<Item> Get();
+        Task<bool> Create();
+        Task<bool> Update();
+        Task<bool> Delete();
+    }
+}
