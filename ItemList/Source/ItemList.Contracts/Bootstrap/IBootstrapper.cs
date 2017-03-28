65276453e73a3670b088f8d24be3c97b516bf49a
Jiri Lojda <JiriL@kentico.com>
2017-03-28 12:49:59 +0200

    KA-86 Create IBootstrapper interface

---
+++ ItemList/Source/ItemList.Contracts/Bootstrap/IBootstrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using ItemList.Contracts.Api;
+
+namespace ItemList.Contracts.Bootstrap
+{
+    public interface IBootstrapper
+    {
+        void RegisterTypes(IIoCContainer container);
+    }
+}
