38ac47819b5f3baa9c9c7360e9150ce058c050e6
Jiri Lojda <JiriL@kentico.com>
2017-04-04 15:18:09 +0200

    KA-86 Create IResolveBootrappersTypes

--- ItemList/Source/ItemList.Contracts/Bootstrap/IIncludeBootstrapper.cs
+++ ItemList/Source/ItemList.Contracts/Bootstrap/IIncludeBootstrapper.cs
@@ -1,6 +1,6 @@
 ﻿namespace ItemList.Contracts.Bootstrap
 {
-    interface IIncludeBootstrapper<out TBootstrapper> 
+    public interface IIncludeBootstrapper<out TBootstrapper> 
         where TBootstrapper : IIncludeBootstrapper<TBootstrapper>
     {
         TBootstrapper Include<TRegistration>() where TRegistration : IBootstrapper, new();
---
+++ ItemList/Source/ItemList.Contracts/Bootstrap/IResolveBootrappersTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web.Http;
+using ItemList.Contracts.Api;
+
+namespace ItemList.Contracts.Bootstrap
+{
+    public interface IResolveBootrappersTypes : IIncludeBootstrapper<IResolveBootrappersTypes>
+    {
+        Action<HttpConfiguration> RegisterAll(IIoCContainer container);
+    }
+}
