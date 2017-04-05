e2bed6346a51d08b3f4395da8b46e1cac5aeef0f
Jiri Lojda <JiriL@kentico.com>
2017-04-05 16:20:33 +0200

    KA-87 Connect repository to MongoDB

--- ItemList/Source/ItemList.Database/Repositories/ItemsRepository.cs
+++ ItemList/Source/ItemList.Database/Repositories/ItemsRepository.cs
@@ -1,31 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ItemList.Contracts.Api;
 using ItemList.Contracts.Database;
 using ItemList.Contracts.Models;
+using MongoDB.Driver;
 
 namespace ItemList.Database.Repositories
 {
     public class ItemsRepository : IItemsRepository
     {
+        private readonly IMongoCollection<Item> _collection;
+
+        public ItemsRepository(IDatabaseConfiguration databaseConfiguration)
+        {
+            var client = new MongoClient(databaseConfiguration.GetConnectionString());
+            var database = client.GetDatabase("itemlist");
+            _collection = database.GetCollection<Item>("Items");
+        }
         public async Task<IEnumerable<Item>> GetAllAsync()
         {
-            IEnumerable<Item> items = new List<Item>()
-            {
-                new Item {Id = new Guid("7383243d-9230-4a6c-94ea-122e151208ca"), Value = "text1"},
-                new Item {Id = new Guid("83aa9154-2b5f-49b7-b7af-25cab7bf2159"), Value = "text2"}
-            };
-            return await Task.FromResult(items);
+            var result = await _collection.FindAsync(FilterDefinition<Item>.Empty);
+            return result.ToEnumerable();
         }
 
         public async Task<Item> GetAsync(Guid id)
         {
-            return await Task.FromResult(new Item { Id = new Guid("331c43f5-11af-43a4-83d1-7d949ae5a8d7"), Value = "text3" });
+            var result = await _collection.FindAsync(item => item.Id == id);
+            return result.FirstOrDefault();
         }
 
         public async Task CreateAsync(Item item)
         {
-            await Task.CompletedTask;
+            await _collection.InsertOneAsync(item);
         }
 
         public async Task UpdateAsync(Item item)
