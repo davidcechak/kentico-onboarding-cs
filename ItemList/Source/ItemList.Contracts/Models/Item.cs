e66db70619ba79eb8d7bf2aa890434707b3251d9
Jiri Lojda <JiriL@kentico.com>
2017-04-05 16:31:28 +0200

    KA-86 Add Created and LastUpdated properties into Item

--- ItemList/Source/ItemList.Contracts/Models/Item.cs
+++ ItemList/Source/ItemList.Contracts/Models/Item.cs
@@ -10,9 +10,13 @@ namespace ItemList.Contracts.Models
 
         public string Value { get; set; }
 
+        public DateTime Created { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
         public override string ToString()
         {
-            return $"{nameof(Id)}: {Id}, {nameof(Ueid)}: {Ueid}, {nameof(Value)}: {Value}";
+            return $"{nameof(Id)}: {Id}, {nameof(Ueid)}: {Ueid}, {nameof(Value)}: {Value}, {nameof(Created)}: {Created}, {nameof(LastUpdated)}: {LastUpdated}";
         }
     }
 }
