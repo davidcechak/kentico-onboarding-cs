c3466b9e5108ee9f754e4b581441a1208909135c
Jiri Lojda <JiriL@kentico.com>
2017-04-05 16:54:45 +0200

    KA-87 Update ItemEqualityComparer according to Item model changes

--- ItemList/Tests/Contracts.Tests.Base/EqualContraintExtensions.cs
+++ ItemList/Tests/Contracts.Tests.Base/EqualContraintExtensions.cs
@@ -1,33 +1,37 @@
 using System;
 using System.Collections.Generic;
+using ItemList.Contracts.Models;
 using NUnit.Framework.Constraints;
 
-namespace Contracts.Tests.Base.Item
+namespace Contracts.Tests.Base
 {
     internal static class EqualContraintExtensions
     {
-        private sealed class ItemEqualityComparer : IEqualityComparer<ItemList.Contracts.Models.Item>
+        private sealed class ItemEqualityComparer : IEqualityComparer<Item>
         {
-            public bool Equals(ItemList.Contracts.Models.Item x, ItemList.Contracts.Models.Item y)
+            public bool Equals(Item x, Item y)
             {
                 if (ReferenceEquals(x, y)) return true;
                 if (ReferenceEquals(x, null)) return false;
                 if (ReferenceEquals(y, null)) return false;
                 if (x.GetType() != y.GetType()) return false;
-                return x.Id.Equals(y.Id) && string.Equals((string) x.Ueid, (string) y.Ueid) && string.Equals((string) x.Value, (string) y.Value);
+                return x.Id.Equals(y.Id) && string.Equals(x.Ueid, y.Ueid) && string.Equals(x.Value, y.Value) && x.Created.Equals(y.Created) && x.LastUpdated.Equals(y.LastUpdated);
             }
 
-            public int GetHashCode(ItemList.Contracts.Models.Item obj)
+            public int GetHashCode(Item obj)
             {
                 unchecked
                 {
                     var hashCode = obj.Id.GetHashCode();
-                    hashCode = (hashCode * 397) ^ (obj.Ueid?.GetHashCode() ?? 0);
-                    hashCode = (hashCode * 397) ^ (obj.Value?.GetHashCode() ?? 0);
+                    hashCode = (hashCode * 397) ^ (obj.Ueid != null ? obj.Ueid.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (obj.Value != null ? obj.Value.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ obj.Created.GetHashCode();
+                    hashCode = (hashCode * 397) ^ obj.LastUpdated.GetHashCode();
                     return hashCode;
                 }
             }
         }
+
 
         private static readonly Lazy<ItemEqualityComparer> ItemEquilityComparer = new Lazy<ItemEqualityComparer>();
 
