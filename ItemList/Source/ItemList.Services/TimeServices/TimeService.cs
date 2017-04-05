4dac69d038796758b64fd8e180e9429acb7fde78
Jiri Lojda <JiriL@kentico.com>
2017-04-05 16:57:27 +0200

    KA-87 Create TimeService

---
+++ ItemList/Source/ItemList.Services/TimeServices/TimeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ItemList.Contracts.Services;
+
+namespace ItemList.Services.TimeServices
+{
+    public class TimeService : ITimeService
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
