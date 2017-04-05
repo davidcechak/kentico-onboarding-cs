3bd2a96012ccddf4e46100725ed32fbc914cb015
Jiri Lojda <JiriL@kentico.com>
2017-04-05 14:40:37 +0200

    KA-87 Create interface and class for getting connection strings

---
+++ ItemList/Source/ItemList.Api/ConnectionStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using ItemList.Contracts.Api;
+
+namespace ItemList.Api
+{
+    public class ConnectionStrings : IConnectionStrings
+    {
+        private static Dictionary<string, string> _connectionString;
+
+        public string GetConnectionString(string name)
+        {
+            string connectionString;
+            if (!_connectionString.TryGetValue(name, out connectionString))
+            {
+                var connectionStringSettings = ConfigurationManager.ConnectionStrings[name];
+                if (connectionStringSettings == null)
+                {
+                    throw new ArgumentException("Name not found in configuration.");
+                }
+                connectionString = connectionStringSettings.ConnectionString;
+                _connectionString.Add(name, connectionString);
+            }
+            return connectionString;
+        }
+    }
+}
---
+++ ItemList/Source/ItemList.Contracts/Api/IConnectionStrings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ItemList.Contracts.Api
+{
+    public interface IConnectionStrings
+    {
+        string GetConnectionString(string name);
+    }
+}
